66f8c0b6909e4fdfac4ad798b734a14902a054ba
Nikolskiy Vladimir <50186930+nikolskiyv@users.noreply.github.com>
2023-04-15 14:54:19 +0300

    fix testing ci

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
-.PHONY: build run lint test
-
+.PHONY: build
 build:
-	docker-compose build
+	docker-compose -f docker-compose.yml build
 
-run:
-	docker-compose build && docker-compose up -d
+.PHONY: run
+run_all_docker:
+	docker-compose -f docker-compose.yml build && \
+	docker-compose -f docker-compose.yml up -d
 
+.PHONY: lint
 lint:
 	flake8 src
 
+.PHONY: test
 test:
-	docker-compose up --build tests
+	docker-compose -f docker-compose.yml up --build --exit-code-from tests
 
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -6,11 +6,3 @@ def test_health_check(client):
     assert response.status_code == 200
     assert response.json == {'status': 'ok'}
 
-
-def test_create_value(client, test_data):
-    # Ensure the value does not exist
-    response = client.get(f'/api/users/{test_data["user_id"]}/keys/{test_data["key"]}')
-    assert response.status_code == HTTPStatus.NOT_FOUND
-
-    response = client.post(f'/api/users/{test_data["user_id"]}', json=test_data)
-    assert response.status_code == HTTPStatus.CREATED
